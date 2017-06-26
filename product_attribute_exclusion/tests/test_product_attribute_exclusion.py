990d2682c25ec29df51a2be27e4f7efd84f6ef56
Graeme Gellatly <gdgellatly@gmail.com>
2017-06-26 16:17:19 +1200

    [FIX] rename product_combination_exclude -> product_attribute_exclusion

--- product_attribute_exclusion/tests/test_product_attribute_exclusion.py
+++ product_attribute_exclusion/tests/test_product_attribute_exclusion.py
@@ -104,37 +104,37 @@ class TestProductCombinationExclude(TransactionCase):
         #  This matrix says we cannot have Green and Black T-Shirts
         #  in medium size
         self.matrix1 = self.browse_ref(
-            'product_combination_exclude.exclude_matrix_test_1')
+            'product_attribute_exclusion.exclude_matrix_test_1')
 
         #  This matrix says we cannot have Green T-Shirts
         #  in large size
         self.matrix2 = self.browse_ref(
-            'product_combination_exclude.exclude_matrix_test_2')
+            'product_attribute_exclusion.exclude_matrix_test_2')
 
         #  all 3 products have the same attribute values, but different
         #  exclusion matrices
         self.product1 = self.browse_ref(
-            'product_combination_exclude.product_exclude_test_1')
+            'product_attribute_exclusion.product_exclude_test_1')
         self.product2 = self.browse_ref(
-            'product_combination_exclude.product_exclude_test_2')
+            'product_attribute_exclusion.product_exclude_test_2')
         self.product3 = self.browse_ref(
-            'product_combination_exclude.product_exclude_test_3')
+            'product_attribute_exclusion.product_exclude_test_3')
         self.product_obj = self.env['product.product']
 
         self.green = self.browse_ref(
-            'product_combination_exclude.product_attribute_value_7')
+            'product_attribute_exclusion.product_attribute_value_7')
         self.purple = self.browse_ref(
-            'product_combination_exclude.product_attribute_value_9')
+            'product_attribute_exclusion.product_attribute_value_9')
         self.medium = self.browse_ref(
-            'product_combination_exclude.product_attribute_value_2')
+            'product_attribute_exclusion.product_attribute_value_2')
         self.large = self.browse_ref(
-            'product_combination_exclude.product_attribute_value_3')
+            'product_attribute_exclusion.product_attribute_value_3')
 
         self.attr_line_2 = self.browse_ref(
-            'product_combination_exclude.product_attribute_line_2')
+            'product_attribute_exclusion.product_attribute_line_2')
 
         self.product_attr2 = self.browse_ref(
-            'product_combination_exclude.product_attribute_2'
+            'product_attribute_exclusion.product_attribute_2'
         )
 
         self.partner = self.env.ref('base.res_partner_1')
