3d02f68be75ae4210f1513e4ae19c75a01213e55
Graeme Gellatly <gdgellatly@gmail.com>
2017-08-24 19:09:15 +1200

    {PORT] V11 product_code_builder

--- product_code_builder/__manifest__.py
+++ product_code_builder/__manifest__.py
@@ -1,23 +1,9 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see http://www.gnu.org/licenses/.
-#
-##############################################################################
+# Copyright 2014- Odoo Community Association - OCA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Product Variant Default Code",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Odoo Community Association (OCA)",
     "contributors": [
         "OdooMRP team",
@@ -31,7 +17,6 @@
     "category": "Product",
     "website": "http://www.odoo-community.org",
     "depends": ['product',
-                'product_attribute_priority'
                 ],
     "data": ['views/product_attribute_value_view.xml',
              'views/product_view.xml',
